7d2a5770a39f910e6e836fd11a5c8140cda863a1
Hugo Quiñónez <hugo_2555@hotmail.com>
2018-09-12 23:33:23 -0600

    Home Page

--- Disosa/wwwroot/js/site.js
+++ Disosa/wwwroot/js/site.js
@@ -1,1 +1,31 @@
 ﻿// Write your JavaScript code.
+function OcultarImagenes() {
+        $(".carrusel-image").css("display", "none");
+    }
+
+    function MostrarImagen(imagen) {
+        OcultarImagenes();
+        var elemento = $("#" + imagen);
+        elemento.fadeIn("1500");
+        elemento.children().css("opacity","0").css("left","0px")
+        .delay(500)
+        .animate({left: '15%', opacity: "1"}, "slow");
+    }
+
+    OcultarImagenes();
+
+    var imagenActual = "imagen1";
+    MostrarImagen(imagenActual);
+    setInterval(function () {
+        if (imagenActual == "imagen1") {
+            imagenActual = "imagen2";
+        } else if (imagenActual == "imagen2") {
+            imagenActual = "imagen3";
+        } else if (imagenActual == "imagen3") {
+            imagenActual = "imagen1"
+        }
+        MostrarImagen(imagenActual);
+    },
+        5000
+    );
+
