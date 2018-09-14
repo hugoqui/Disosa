dfbbdb2c322e20706c3714cf9eecc80fc8fb8455
Hugo Quiñonez <hugo_2555@hotmail.com>
2018-09-14 17:15:57 -0600

    products

--- DisosaWebSite/Controllers/HomeController.cs
+++ DisosaWebSite/Controllers/HomeController.cs
@@ -1,14 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data;
 using System.Linq;
-using System.Web;
+using DisosaWebSite.Models;
 using System.Web.Mvc;
+using PagedList;
 
 namespace DisosaWebSite.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        private disosadbEntities db = new disosadbEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -22,6 +24,20 @@ namespace DisosaWebSite.Controllers
         public ActionResult Contact()
         {
             return View();
+        }
+
+        public ActionResult Products(string search, int? page)
+        {
+            if (search != null) { page = 1; } // cuando haya algo, que muestre la pagina 1
+            var productos = db.Productos.ToList();
+            if (!String.IsNullOrEmpty(search))
+            {
+                productos = productos.Where(p => p.Nombre.Contains(search)).ToList();
+            }
+            productos = productos.OrderBy(p => p.Codigo).ToList();
+            int pageSize = 12;
+            int pageNumber = (page ?? 1);
+            return View(productos.ToPagedList(pageNumber, pageSize));
         }
     }
 }
